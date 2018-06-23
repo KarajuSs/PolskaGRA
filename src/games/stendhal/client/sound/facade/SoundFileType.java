1d639428fd3fddae43cceef6ab93d59ee26f9d0a
KarajuSs <karajussgg@gmail.com>
2018-06-23 14:50:54 +0200

    Revert "upload via files"
    This reverts commit f815a4a01fcb7d77f1e6cc72b6e37f334c20d433.

--- src/games/stendhal/client/sound/facade/SoundFileType.java
+++
@@ -1,6 +0,0 @@
-/* $Id$
- * $Log$
- */
-package games.stendhal.client.sound.facade;
-
-public enum SoundFileType { OGG, WAV }
--- wordlist.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call ant compile_stendhaltools server_build
-
-set LOCALCLASSPATH=build\build_stendhaltools;build\build_server;libs\marauroa.jar;libs\log4j.jar;libs\mysql-connector-java-5.1.jar;libs\h2.jar
-javaw -cp "%LOCALCLASSPATH%" games.stendhal.tools.npcparser.WordListUpdate
