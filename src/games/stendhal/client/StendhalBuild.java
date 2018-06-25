0c1f8a9c262304e7b8241f9d4b940fa8c03ec687
KarajuSs <karajussgg@gmail.com>
2018-06-25 14:50:13 +0200

    Poprawki kodu

---
+++ src/games/stendhal/client/StendhalBuild.java
@@ -0,0 +1,7 @@
+
+			package games.stendhal.client;
+			public class StendhalBuild {
+			private static final String BUILD_NUMBER = "2018-06-25 13:58:20";
+			public static String getBuildNumber() {return BUILD_NUMBER;}
+			}
+		
