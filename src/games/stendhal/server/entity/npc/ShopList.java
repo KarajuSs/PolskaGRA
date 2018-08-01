8f592352d2b278d1b03d8d9043c292f82820f308
KarajuSs <karajussgg@gmail.com>
2018-08-01 23:37:20 +0200

    Zmiany w sklepie Juhasa

--- src/games/stendhal/server/entity/npc/ShopList.java
+++ src/games/stendhal/server/entity/npc/ShopList.java
@@ -595,13 +595,14 @@ public final class ShopList {
 		
 		//Juhas sprzedaje
 		shops.add("juhas", "zwój tatrzański", 250);
-		shops.add("juhas", "zwój krakowski", 400);
-		shops.add("juhas", "zwój wieliczka", 600);
+		shops.add("juhas", "zwój semos", 300);
+		//shops.add("juhas", "zwój krakowski", 400);
+		//shops.add("juhas", "zwój wieliczka", 600);
 		shops.add("juhas", "zwój ados", 800);
 		shops.add("juhas", "zwój fado", 900);
 		shops.add("juhas", "zwój kalavan", 1000);
 		shops.add("juhas", "zwój kirdneh", 1000);
-		shops.add("juhas", "bilet turystyczny", 5000);
+		//shops.add("juhas", "bilet turystyczny", 5000);
 	}
 
 	private static ShopList instance;
