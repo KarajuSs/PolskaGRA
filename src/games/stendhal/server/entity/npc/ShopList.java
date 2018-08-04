4fcb40a81e98810c5adaf06f729f28c63cbcbc7e
KarajuSs <karajussgg@gmail.com>
2018-08-04 12:45:39 +0200

    Juhas sprzedaje bilet turystyczny

--- src/games/stendhal/server/entity/npc/ShopList.java
+++ src/games/stendhal/server/entity/npc/ShopList.java
@@ -603,7 +603,7 @@ public final class ShopList {
 		shops.add("juhas", "zwój fado", 900);
 		shops.add("juhas", "zwój kalavan", 1000);
 		shops.add("juhas", "zwój kirdneh", 1000);
-		//shops.add("juhas", "bilet turystyczny", 5000);
+		shops.add("juhas", "bilet turystyczny", 5000);
 	}
 
 	private static ShopList instance;
