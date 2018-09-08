ed23527e99c658cde0a2aad1fcbf17da89c3be82
KarajuSs <karajussgg@gmail.com>
2018-09-08 14:02:22 +0200

    Przeniesienie NPC z "outskirts" & "fishinghut" do folderu "planty"

--- src/games/stendhal/server/maps/krakow/planty/KajetanNPC.java
+++ src/games/stendhal/server/maps/krakow/planty/KajetanNPC.java
@@ -9,14 +9,13 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-package games.stendhal.server.maps.krakow.outskirts;
+package games.stendhal.server.maps.krakow.planty;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import games.stendhal.server.core.config.ZoneConfigurator;
-import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
@@ -46,7 +45,14 @@ public class KajetanNPC implements ZoneConfigurator {
 			@Override
 			protected void createPath() {
 				final List<Node> nodes = new LinkedList<Node>();
-				nodes.add(new Node(x, y)); // CHWILOWO BRAK ROZPLANOWANIA
+				nodes.add(new Node(103, 110));
+				nodes.add(new Node(110, 110));
+				nodes.add(new Node(110, 111));
+				nodes.add(new Node(111, 111));
+				nodes.add(new Node(111, 112));
+				nodes.add(new Node(112, 112));
+				nodes.add(new Node(112, 113));
+				nodes.add(new Node(117, 113));
 				setPath(new FixedPath(nodes, true));
 			}
 
@@ -62,7 +68,7 @@ public class KajetanNPC implements ZoneConfigurator {
 
 		npc.setDescription("Oto Kajetan. Zajmuje się wypasaniem kóz.");
 		npc.setEntityClass("noimagenpc"); // pasterz
-		npc.setPosition(x, y); // CHWILOWO BRAK ROZPLANOWANIA
+		npc.setPosition(106, 110);
 		npc.initHP(100);
 		zone.add(npc);
 	}
--- src/games/stendhal/server/maps/krakow/planty/ThomasNPC.java
+++ src/games/stendhal/server/maps/krakow/planty/ThomasNPC.java
@@ -9,14 +9,13 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-package games.stendhal.server.maps.krakow.fishinghut;
+package games.stendhal.server.maps.krakow.planty;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import games.stendhal.server.core.config.ZoneConfigurator;
-import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
@@ -46,7 +45,14 @@ public class ThomasNPC implements ZoneConfigurator {
 			@Override
 			protected void createPath() {
 				final List<Node> nodes = new LinkedList<Node>();
-				nodes.add(new Node(x, y)); // CHWILOWO BRAK ROZPLANOWANIA
+				nodes.add(new Node(78, 51));
+				nodes.add(new Node(78, 53));
+				nodes.add(new Node(77, 53));
+				nodes.add(new Node(77, 55));
+				nodes.add(new Node(73, 55));
+				nodes.add(new Node(75, 55));
+				nodes.add(new Node(75, 54));
+				nodes.add(new Node(77, 54));
 				setPath(new FixedPath(nodes, true));
 			}
 
@@ -62,7 +68,7 @@ public class ThomasNPC implements ZoneConfigurator {
 
 		npc.setDescription("Oto Thomas. Zajmuje się łowieniem ryb rzecznych, jednak codziennie wypływa również do portu, gdzie czeka na niego świeża dostawa tuńczyka. Tuńczyk to morska ryba, której nie złowi się w rzece, a jest na niego spore zapotrzebowanie.");
 		npc.setEntityClass("fishermannpc");
-		npc.setPosition(x, y); // CHWILOWO BRAK ROZPLANOWANIA
+		npc.setPosition(79, 51);
 		npc.initHP(100);
 		zone.add(npc);
 	}
