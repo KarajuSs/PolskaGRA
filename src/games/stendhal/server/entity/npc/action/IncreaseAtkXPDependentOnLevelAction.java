8ffc252c9a04bbfeba11925570e187791d30093b
KarajuSs <karajussgg@gmail.com>
2018-09-26 12:25:40 +0200

    Zmniejszono ilość dostawanych punktów umiejętności za zdobytego EXP'a z 10% na 5%

--- src/games/stendhal/server/entity/npc/action/IncreaseAtkXPDependentOnLevelAction.java
+++ src/games/stendhal/server/entity/npc/action/IncreaseAtkXPDependentOnLevelAction.java
@@ -45,7 +45,7 @@ public class IncreaseAtkXPDependentOnLevelAction implements ChatAction {
 	public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
 		final int start = Level.getXP(player.getLevel());
 		final int next = Level.getXP(player.getLevel() + 1);
-		int reward = (int) ((next - start) / (atk_xpDiff) / 10);
+		int reward = (int) (((next - start) / (atk_xpDiff) / 10) / 2);
 		player.setAtkXP(reward + player.getAtkXP());
 		player.notifyWorldAboutChanges();
 	}
--- src/games/stendhal/server/entity/npc/action/IncreaseDefXPDependentOnLevelAction.java
+++ src/games/stendhal/server/entity/npc/action/IncreaseDefXPDependentOnLevelAction.java
@@ -46,7 +46,7 @@ public class IncreaseDefXPDependentOnLevelAction implements ChatAction {
 	public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
 		final int start = Level.getXP(player.getLevel());
 		final int next = Level.getXP(player.getLevel() + 1);
-		int reward = (int) ((next - start) / (def_xpDiff) / 10);
+		int reward = (int) (((next - start) / (def_xpDiff) / 10) / 2);
 		player.setDefXP(reward + player.getDefXP());
 		player.notifyWorldAboutChanges();
 	}
